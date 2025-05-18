4677a37d573193c844ed8a8fda5e297e58c6fe91
REJ <rejbascoguinb@gmai.com>
2025-05-18 17:43:41 +0800

    Deploy site to main branch root

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/rej_coffee/', // <-- use your exact repo name here
+  base: '/rej_coffee/',  // <-- exactly your repo name with slashes
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
 });
